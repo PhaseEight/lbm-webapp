000e2bf10328416401c7bc6f2631b700cb8e1616
Peter Neil as pedroneil <pedroneil@gmail.com>
2013-09-26 11:16:01 +0100

    upgraded to Spring 4.0.0M3 and SWF2.4.0M1

---
+++ src/main/java/com/logbookmanager/jmx/beans/HelloJMX.java
@@ -0,0 +1,25 @@
+package com.logbookmanager.jmx.beans;
+
+import org.springframework.jmx.export.annotation.ManagedAttribute;
+import org.springframework.jmx.export.annotation.ManagedResource;
+import org.springframework.stereotype.Component;
+
+@Component
+@ManagedResource(objectName = "bean:name=HelloSpringJMX")
+public class HelloJMX {
+
+	String message = null;
+ 
+	@ManagedAttribute(description = "get the message")
+	public String getMessage() {
+
+		return this.message;
+	}
+
+	@ManagedAttribute(description = "set the message")
+	public void setMessage(String Message) {
+
+		this.message = Message;
+
+	}
+}
